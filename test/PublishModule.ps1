b615de3cae2a0be135e075a72ba3a5ab2e5fd66a
Igor Iric <iiric@microsoft.com>
2020-12-20 16:11:07 +0100

    redone dotnet publish

--- test/PublishModule.ps1
+++ test/PublishModule.ps1
@@ -5,17 +5,17 @@
 Write-Output "dotnet publish"
 
 # https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
-$runtime = " --runtime " + (($IsLinux -or $IsMacOS) ? "linux-x64" : "win-x64")
-dotnet publish src/azmi/azmi.csproj $runtime
+$runtime = (($IsLinux -or $IsMacOS) ? "linux-x64" : "win-x64")
+dotnet publish 'src/azmi/azmi.csproj'  --runtime $runtime
 
 # Find proper dll to import
 # idea taken from https://www.michev.info/Blog/Post/1722/use-powershell-to-list-all-directories-that-contain-both-file1-and-file2
-$dirs = (Get-ChildItem -Include 'azmi.dll', 'System.Management.Automation.dll' -Recurse | Group Directory | ? Count -eq 2).Name
+$files = @('azmi.dll', 'System.Management.Automation.dll')
+$dirs = (Get-ChildItem -Include $files -Recurse | Group Directory | ? Count -eq 2).Name
 
 if ($null -eq $dirs) {
-	# We did not find proper dll to import
-	# Display troubleshooting information
-	@('azmi.dll', 'System.Management.Automation.dll') | % {
+	Write-Warning 'We did not find proper dll to import, display troubleshooting information'
+	$files | % {
 		$_
 		gci $_ -Recurse
 	}
