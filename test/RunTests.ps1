9a47499ea0dbce6423e9a7fb54aafe1badacd4a2
Igor Iric <iiric@microsoft.com>
2020-12-20 16:48:51 +0100

    more verbose testing

--- test/RunTests.ps1
+++ test/RunTests.ps1
@@ -1,7 +1,10 @@
 ./test/PublishModule.ps1
 
 if (!(Get-Module 'Pester' -List | ? Version -ge 5.0.0)) {
+	Write-Output "Import Pester 5 module"
 	Install-Module 'Pester' -RequiredVersion 5.0.0
 }
 
+
+Write-Output "Invoke Pester tests"
 Invoke-Pester -CI
