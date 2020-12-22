1cc135095605f833dd6480c18523f3a892197877
Igor Iric <iiric@microsoft.com>
2020-12-22 17:26:51 +0100

    add import and run tests script

---
+++ test/Import'n'RunTests.ps1
@@ -0,0 +1,20 @@
+$modulePath = './publish/azmi.dll'
+
+if (get-item $modulePath) {
+	Write-Output "Import azmi module"
+	Import-Module $modulePath -Force
+} else {
+	Write-Output "azmi module not found, searching for it..."
+	Get-ChildItem -Recurse
+	exit 1
+}
+
+
+if (!(Get-Module 'Pester' -List | ? Version -ge 5.0.0)) {
+	Write-Output "Import Pester 5 module"
+	Install-Module 'Pester' -RequiredVersion 5.0.0
+}
+
+
+Write-Output "Invoke Pester tests"
+Invoke-Pester -CI
