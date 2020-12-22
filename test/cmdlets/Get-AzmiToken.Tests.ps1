79f627a149c3f38270d1aee5b7641c5f452e4179
Igor Iric <iiric@microsoft.com>
2020-12-22 18:36:55 +0100

    extended get-token tests

--- test/cmdlets/Get-AzmiToken.Tests.ps1
+++ test/cmdlets/Get-AzmiToken.Tests.ps1
@@ -2,6 +2,9 @@ BeforeAll {
 
     $moduleName = 'azmi'
     $commandName = 'Get-AzmiToken'
+    $managedIdentityName = 'azmitest'
+
+
     if (!(Get-Module $moduleName)) {
         throw "You must import module before running tests."
     }
@@ -18,6 +21,33 @@ Describe 'Function import verifications'  {
     It 'Function has identity argument' {
         $P = (Get-Command $commandName -Module $moduleName).Parameters
         $P.Identity | Should -Not -BeNullOrEmpty
+    }
+}
+
+Describe 'Basic tests'  {
+
+    It 'It returns something' {
+        Get-AzmiToken  | Should -Not -BeNullOrEmpty
+    }
+
+    It 'It accepts JWTformat switch' {
+        Get-AzmiToken -JWTformat | Should -Not -BeNullOrEmpty
+    }
+
+    It 'It accepts JWTformat switch' {
+        Get-AzmiToken -JWTformat | Should -Contain $managedIdentityName
+    }
+
+}
+
+Describe 'Identity argument'  {
+
+    It 'It works with proper ID' {
+        # Get-AzmiToken  | Should -Not -BeNullOrEmpty
+    }
+
+    It 'It fails with fake ID' {
+        {Get-AzmiToken -Identity fakeIdentity}| Should -Throw
     }
 
 }
