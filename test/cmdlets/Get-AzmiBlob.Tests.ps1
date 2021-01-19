a37334817c874d5694bc8076e3d48662c03d839c
Igor Iric <iiric@microsoft.com>
2021-01-19 16:57:29 +0100

    added basic list blob container tests

--- test/cmdlets/Get-AzmiBlob.Tests.ps1
+++ test/cmdlets/Get-AzmiBlob.Tests.ps1
@@ -13,6 +13,14 @@ BeforeAll {
 
     # import environment variables
     $MSI = $Env:IDENTITY_CLIENT_ID
+    $STORAGEACCOUNTNAME = $Env:STORAGE_ACCOUNT_NAME
+
+    # calculated variables
+    $CONTAINER_NA="https://$STORAGEACCOUNTNAME.blob.core.windows.net/azmi-na"
+    $CONTAINER_RO="https://$STORAGEACCOUNTNAME.blob.core.windows.net/azmi-ro"
+    $CONTAINER_RW="https://$STORAGEACCOUNTNAME.blob.core.windows.net/azmi-rw"
+    $CONTAINER_LB="https://$STORAGEACCOUNTNAME.blob.core.windows.net/azmi-ls"
+
 
     if (!(Get-Module $moduleName)) {
         throw "You must import module before running tests."
@@ -30,4 +38,25 @@ Describe 'Function import verifications'  {
         $P = (Get-Command $commandName -Module $moduleName).Parameters
         $P.Identity | Should -Not -BeNullOrEmpty
     }
+}
+
+
+Describe 'Basic tests'  {
+
+    It 'It returns something' {
+        Get-AzmiBlob  | Should -Not -BeNullOrEmpty
+    }
+}
+
+
+Describe 'Identity argument'  {
+
+    It 'It works with proper ID' {
+        Get-AzmiBlob -Container $CONTAINER_LB -Identity $MSI | Should -Not -BeNullOrEmpty
+    }
+
+    It 'It fails with fake ID' {
+        {Get-AzmiToken -Container $CONTAINER_LB -Identity fakeIdentity}| Should -Throw
+    }
+
 }
