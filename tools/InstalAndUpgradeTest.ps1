e1f45b17f9d192bda9806c0e7b56a83ee2ee8c24
Igor Iric <iricigor@outlook.com>
2021-03-16 18:23:07 +0100

    test file updated to include verifications

--- tools/InstalAndUpgradeTest.ps1
+++ tools/InstalAndUpgradeTest.ps1
@@ -1,13 +1,33 @@
-$module = 'azmi'
+$moduleName = 'azmi'
 
-# install and remove
-Install-Module $module -Repository PSGallery -Scope CurrentUser -Force
-Get-Module $module -List
-Uninstall-Module $module -Force
-Get-Module $module -List -ea 0
+# install module
+Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -Force -MinimumVersion 0.0.2
+if (Get-Module $moduleName -List | ? Version -gt 0.0.1) {
+    Write-Host "Module installed successfully."
+} else {
+    Write-Error "Module not installed successfully."
+}
 
-# upgrade
-Install-Module $module -Repository PSGallery -Scope CurrentUser -RequiredVersion 0.0.1 -Force
-Get-Module $module -List
-Update-Module $module -Force
-Get-Module $module -List
+# remove module
+Uninstall-Module $moduleName -Force
+if (Get-Module $moduleName -List -ea 0 | ? Version -gt 0.0.1) {
+    Write-Error "Module not uninstalled successfully."
+} else {
+    Write-Host "Module uninstalled successfully."
+}
+
+# install old version
+Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -RequiredVersion 0.0.1 -Force
+if (Get-Module $moduleName -List | ? Version -eq 0.0.1) {
+    Write-Host "Old module installed successfully."
+} else {
+    Write-Error "Old module not installed successfully."
+}
+
+# upgrade module to latest
+Update-Module $moduleName -Force
+if (Get-Module $moduleName -List | ? Version -gt 0.0.1) {
+    Write-Host "Module updated successfully."
+} else {
+    Write-Error "Module not updated successfully."
+}
