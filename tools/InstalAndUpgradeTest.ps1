b244575b08ef68b0ddd9ac498a468eea32dfef0c
Igor Iric <iricigor@outlook.com>
2021-03-16 18:32:24 +0100

    more verbose output

--- tools/InstalAndUpgradeTest.ps1
+++ tools/InstalAndUpgradeTest.ps1
@@ -1,31 +1,31 @@
 $moduleName = 'azmi'
 
-# install module
-Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -Force -MinimumVersion 0.0.2
+Write-Host "Install module"
+Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -Force -MinimumVersion 0.0.2 -Verbose
 if (Get-Module $moduleName -List | ? Version -gt 0.0.1) {
     Write-Host "Module installed successfully."
 } else {
     Write-Error "Module not installed successfully."
 }
 
-# remove module
-Uninstall-Module $moduleName -Force
+Write-Host "Uninstall module"
+Uninstall-Module $moduleName -Force -Verbose
 if (Get-Module $moduleName -List -ea 0 | ? Version -gt 0.0.1) {
     Write-Error "Module not uninstalled successfully."
 } else {
     Write-Host "Module uninstalled successfully."
 }
 
-# install old version
-Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -RequiredVersion 0.0.1 -Force
+Write-Host "Install old version"
+Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -RequiredVersion 0.0.1 -Force -Verbose
 if (Get-Module $moduleName -List | ? Version -eq 0.0.1) {
     Write-Host "Old module installed successfully."
 } else {
     Write-Error "Old module not installed successfully."
 }
 
-# upgrade module to latest
-Update-Module $moduleName -Force
+Write-Host "Update module"
+Update-Module $moduleName -Force -Verbose
 if (Get-Module $moduleName -List | ? Version -gt 0.0.1) {
     Write-Host "Module updated successfully."
 } else {
